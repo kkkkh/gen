34c3e9c25db826791879c93ebc7d35380e98922a
张浩田 <haotian.zhang@seaboxdata.com>
2021-11-29 18:25:44 +0800

    fix vite base /gen/

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import {defineConfig} from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [vue()],
-})
+// console.log('import.meta.env.VITE_APP_BASE_URL', import.meta.env)
+// // console.log('process.env', process.env)
+// // console.log('process.env.VITE_APP_BASE_URL', process.env.VITE_APP_BASE_URL)
+// export default defineConfig({
+// 	plugins: [vue()],
+// 	// base: import.meta.env.VITE_APP_BASE_URL,
+// })
+
+export default ({mode}) => {
+	return defineConfig({
+		plugins: [vue()],
+		base: loadEnv(mode, process.cwd()).VITE_APP_BASE_URL,
+	})
+}
